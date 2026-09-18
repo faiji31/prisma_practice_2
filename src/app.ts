749082de131e316b0f9bbf68108450614fb24869
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-09-18 23:43:37 +0600

    5th commit

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import cookieParser from "cookie-parser";
 import express,{ Application, Request, Response } from "express";
 import cors from 'cors'
 import config from "./config";
+import  httpstatus  from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 
 const app:Application = express()
@@ -18,6 +21,58 @@ app.use(cookieParser())
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("hello world")
+})
+
+app.post("/api/users/register",async(req:Request,res:Response)=>{
+    const {name,email,password,profilePhoto} = req.body
+
+    const isUserexits = await prisma.user.findUnique({
+        where:{email}
+    })
+    if(isUserexits){
+        throw new Error("user is already exits")
+
+    }
+
+    const hashPassword = await bcrypt.hash(password,Number(config.bcrypt_salt_rounds))
+
+    const createduser = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password:hashPassword
+        }
+    })
+
+    await prisma.profile.create({
+        data:{
+            userId:createduser.id,
+            profilePhoto
+        }
+    })
+
+    const user = await prisma.user.findUnique({
+        where:{
+            id:createduser.id,
+            email:createduser.email||email
+
+        },
+        include:{
+            profile:true
+        },
+        omit:{
+            password:true
+        }
+    })
+  
+    res.status(httpstatus.CREATED).json({
+        success:true,
+        statuscode:httpstatus.CREATED,
+        message:"user is register successfully!!",
+        data:{
+            user
+        }
+    })
 })
 
 export default app
