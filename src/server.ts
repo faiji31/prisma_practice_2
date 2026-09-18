3c00daf8f533d6c171db6ebdca789d65ea509795
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-09-18 19:25:14 +0600

    3rd commit

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT||5000;
+const PORT = config.port
 async function main(){
     try {
+        await prisma.$connect()
+        console.log("prisma is connect successfully!!")
         app.listen(PORT,()=>{
             console.log("Server is running!!")
         })
         
     } catch (error) {
         console.error("error staring the server:",error)
+        await prisma.$disconnect()
         process.exit(1)
         
     }
